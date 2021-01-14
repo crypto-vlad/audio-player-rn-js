4ae4c76c9e1f788f181b18aeacb7e055058b45e5
mobile-dev-hybrid <cutecate2019118@gmail.com>
2021-01-14 16:58:56 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import PlayerScreen from './src/PlayerScreen';
+
+export default function App() {
+    return ( 
+        <View style = { styles.container } >
+            <StatusBar barStyle = "light-content" backgroundColor = "#030303" />
+            <PlayerScreen />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#030303',
+        // alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
